140207cd33bd670db5d3f08f0a7ad01893d1e66e
Prachi Sukre <prachisukre2005@gmail.com>
2025-06-06 16:28:28 +0530

    updated admindashboard

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -27,7 +27,7 @@ const AdminDashboard = () => {
             <div className="flex justify-center">
               <button
                 className="text-white px-4 py-2 rounded bg-gradient-to-r from-purple-700 to-indigo-900 shadow-md transition duration-300 ease-in-out hover:bg-purple-700 focus:ring-2 focus:ring-purple-600"
-                onClick={() => (window.location.href = "/TeamMem")}
+                onClick={() => (window.location.href = "/TeamMem.jsx")}
               >
                 Manage
               </button>
